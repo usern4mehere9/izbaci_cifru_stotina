bd09d1bf159b320c1e6b81937aa4c126795a3d1d
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-02 15:48:56 +0200

    Add files via upload

---
+++ izbaci_cifru_stotina/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace zbir_cifara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a, b, c;
+
+            n = int.Parse(Console.ReadLine());
+
+            a = n / 1000;
+            b = n % 100;
+            c = 100 * a + b;
+
+            Console.WriteLine(c);
+        }
+    }
+}
